fc91bd73c5b54af8ef2c076f37c182b9558b786e
xochitlvaleriacurielaranda-dot <xochitlvaleriacurielaranda@gmail.com>
2026-04-15 13:40:06 -0600

    Implementacion de  equals, hashCode, y compareTo

--- src/problema1/Lote.java
+++ src/problema1/Lote.java
@@ -14,7 +14,21 @@ public class Lote {
     public int getNoPiezas(){
         return noPiezas;
     }
-
+     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Lote)) return false;
+        Lote lote = (Lote) o;
+        return idLote == lote.idLote;
+    }
+     @Override
+    public int hashCode() {
+        return Objects.hash(idLote);
+    }
+     @Override
+    public int compareTo(Lote otro) {
+        return Integer.compare(this.prendas.size(), otro.prendas.size());
+    }
     @Override
     public String toString(){
         return "Lote{" +
