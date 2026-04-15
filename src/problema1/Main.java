20c3bbdafc238da268a0cf7b1bd2e11432e5fcbc
xochitlvaleriacurielaranda-dot <xochitlvaleriacurielaranda@gmail.com>
2026-04-15 13:57:19 -0600

    codigo main
    Added multiple instances of Prenda for comparison and testing.

--- src/problema1/Main.java
+++ src/problema1/Main.java
@@ -4,7 +4,9 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        Prenda prenda= new Prenda("Camisa","Algodón", 200, "Masculino", "Verano");
+        Prenda prenda1= new Prenda("Camisa","Algodón", 200, "Masculino", "Verano");
+        Prenda prenda2= new Prenda("Camisa","Algodón", 200, "Masculino", "Verano");
+        Prenda prenda3= new Prenda("Pantalón","Algodón", 500, "Femenino", "Otoño");
         Lote lote= new Lote(101, 50, LocalDate.now());
 
         Venta venta= new Venta(prenda, lote);
@@ -14,5 +16,8 @@ public class Main {
         System.out.println("Precio por lote: $" + venta.precioPorLote());
         System.out.println("Monto recuperación por prenda: $" + venta.montoRecuperacionPrenda());
         System.out.println("Monto recuperación por lote: $" + venta.montoRecuperacionLote());
+        System.out.println(prenda1.equals(prenda2));
+        System.out.println(prenda1.equals(prenda3));
+        System.out.println(prenda1.hashCode() == prenda2.hashCode());
         }
     }
