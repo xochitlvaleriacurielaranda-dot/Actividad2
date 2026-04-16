59d465b2ad8d2079e62be108a6c2f70ae1e614d3
xochitlvaleriacurielaranda-dot <xochitlvaleriacurielaranda@gmail.com>
2026-04-15 22:05:04 -0600

    correciondos

--- src/problema1/Main.java
+++ src/problema1/Main.java
@@ -4,13 +4,13 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        Prenda prenda1= new Prenda("Camisa","Algodón", 200, "Masculino", "Verano");
-        Prenda prenda2= new Prenda("Camisa","Algodón", 200, "Masculino", "Verano");
-        Prenda prenda3= new Prenda("Pantalón","Algodón", 500, "Femenino", "Otoño");
-        Lote lote= new Lote(101, 50, LocalDate.now());
+        Prenda prenda1 = new Prenda("Camisa","Algodón", 200, "Masculino", "Verano");
+        Prenda prenda2 = new Prenda("Camisa","Algodón", 200, "Masculino", "Verano");
+        Prenda prenda3 = new Prenda("Pantalón","Algodón", 500, "Femenino", "Otoño");
+        Lote lote = new Lote(101, 50, LocalDate.now());
 
-        Venta venta= new Venta(prenda, lote);
-        System.out.println(prenda);
+        Venta venta = new Venta(1, prenda1, lote); // idVenta = 1
+        System.out.println(prenda1);
         System.out.println(lote);
         System.out.println("Precio por pieza: $" + venta.precioPorPrenda());
         System.out.println("Precio por lote: $" + venta.precioPorLote());
@@ -19,5 +19,5 @@ public class Main {
         System.out.println(prenda1.equals(prenda2));
         System.out.println(prenda1.equals(prenda3));
         System.out.println(prenda1.hashCode() == prenda2.hashCode());
-        }
     }
+}
